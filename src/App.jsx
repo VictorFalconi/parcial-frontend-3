3e2c11abe40ecbe21f16bf618d4007fc74244a33
Victor Falconi <v.falconi.m@gmail.com>
2024-08-14 19:59:01 -0500

    Examen finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import Card from "./Components/Card"
+import { useState } from "react";
+import Card from "./Components/Card";
+import AppStyles from "./Styles/App.module.css";
 
 function App() {
+  const [showCard, setShowCard] = useState(false);
+
+  const [customer, setCustomer] = useState({
+    name: "",
+    lastName: "",
+  });
+
+  const [error, setError] = useState(false);
+
+  const handleChange = (e) => {
+      setCustomer({...customer, [e.target.name]: e.target.value});
+      setError(false);
+      setShowCard(false);
+  }
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+      if(customer.name.trim().length >= 3 && customer.lastName.trim().length >= 6) 
+        {setShowCard(true); setError(false);}
+      else {setError(true); setShowCard(false);}
+  }
 
   return (
-    <>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card/>
-    </>
+    <div className={AppStyles.container}>
+      <h1 className={AppStyles.title}>Carga de estudiantes</h1>
+      <div>
+        <form onSubmit={handleSubmit} className={AppStyles.form}>
+        <h3>Ingresa tus datos</h3>
+        <input type="text" value={customer.name} name="name" onChange={handleChange} placeholder="Nombre"/>
+        <input type="text" value={customer.lastName} name="lastName" onChange={handleChange} placeholder="Apellido"/>
+        <button>Enviar</button>
+        </form>
+        {error && <h4 style={{color: "red", marginTop: "10px"}}>Por favor chequea que la información sea correcta</h4>}
+      </div>
+      {showCard && <Card customer={customer}/>}
+    </div>
   )
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,13 @@
-const Card = () => {
-    return <div>Hola, soy una tarjeta</div>;
+import CardStyles from "../Styles/Card.module.css";
+
+const Card = ({customer}) => {
+    const {name, lastName} = customer;
+    return (
+        <div className={CardStyles.container}>
+            <h3>Hola, {name}!</h3>
+            <h4>Tu apellido es {lastName}.</h4>
+        </div>
+    );
 };
 
 export default Card;
